1b4451f1e72159481d1130f0bbd90fa31518d774
Rachel Vasquez <rachelrvasquez@gmail.com>
2024-05-10 19:11:26 +0000

    more mobile tweaks, dynamic hero

--- functions.php
+++ functions.php
@@ -155,8 +155,7 @@ function rachievee_2024_scripts()
 	wp_enqueue_style('rachievee-2024-style', get_stylesheet_uri(), array(), _S_VERSION);
 	wp_style_add_data('rachievee-2024-style', 'rtl', 'replace');
 
-	// wp_enqueue_script('rachievee-2024-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
-	wp_enqueue_script('rachievee-2024-navigation', get_template_directory_uri() . '/js/navigation.js', array(), false, true);
+	wp_enqueue_script('rachievee-2024-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
--- index.php
+++ index.php
@@ -15,63 +15,54 @@
 
 get_header();
 
-if ( is_home() ) { ?>
-
-<div class="site-homepage-hero">
-	<h2 class="hero-heading"><span>Hello,</span> I'm Rachel!</h2>
-	<div class="hero-subtitle">Full-Stack Developer & WordPress Wizard</div>
-	<div class="hero-blurb">I write about accessibility, tutorials, and dev life.</div>
-</div>
-
-<?php } ?>
-
-<?php /* if (is_home()) {
+if ( is_home() ) {
 	$fetch_hero_title = get_option('_homepage_hero_title') ?: '';
 	$fetch_hero_subtitle = get_option('_homepage_hero_subtitle') ?: '';
 	$fetch_hero_blurb = get_option('_homepage_hero_desc') ?: '';
+	$hero_content = $fetch_hero_title && $fetch_hero_subtitle && $fetch_hero_blurb;
 
-	if ($fetch_hero_blurb) { ?>
+	if ( $hero_content ) { ?>
 		<div class="site-homepage-hero">
-			<div class="site-homepage-hero-inner">
-				<h2 class="hero-heading"><?php echo $fetch_hero_title; ?></h2>
-				<div class="hero-subtitle"><?php echo $fetch_hero_subtitle; ?></div>
-				<div class="hero-blurb"><?php echo wpautop($fetch_hero_blurb); ?></div>
-			</div>
+			<h2 class="hero-heading"><?php echo wp_kses( $fetch_hero_title, array(
+				'span' => array()
+			) ); ?></h2>
+			<div class="hero-subtitle"><?php echo esc_html( $fetch_hero_subtitle, 'rachievee-2024' ); ?></div>
+			<div class="hero-blurb"><?php echo wpautop( $fetch_hero_blurb ); ?></div>
 		</div>
 <?php } 
+} //is_home()  ?>
 
-} */ ?>
+<div class="site-grid">
+	<main id="primary" class="site-main">
+		<?php
+		if (have_posts()) :
 
-<main id="primary" class="site-main">
-	<?php
-	if (have_posts()) :
+			/* Start the Loop */
+			while (have_posts()) :
+				the_post();
 
-		/* Start the Loop */
-		while (have_posts()) :
-			the_post();
+				/*
+					* Include the Post-Type-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					*/
+				get_template_part('template-parts/content', get_post_type());
 
-			/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-			get_template_part('template-parts/content', get_post_type());
+			endwhile;
 
-		endwhile;
+			the_posts_navigation(array(
+				'prev_text' => '<i class="fa-solid fa-angles-left" aria-hidden="true"></i> Older Articles',
+				'next_text' => 'Newer Articles <i class="fa-solid fa-angles-right" aria-hidden="true"></i>',
+			));
 
-		the_posts_navigation(array(
-			'prev_text' => '<i class="fa-solid fa-angles-left" aria-hidden="true"></i> Older Articles',
-			'next_text' => 'Newer Articles <i class="fa-solid fa-angles-right" aria-hidden="true"></i>',
-		));
+		else :
 
-	else :
+			get_template_part('template-parts/content', 'none');
 
-		get_template_part('template-parts/content', 'none');
+		endif;
+		?>
+	</main><!-- #main -->
 
-	endif;
-	?>
-</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+	<?php get_sidebar(); ?>
+</div><!-- .site-grid -->
+<?php get_footer();
